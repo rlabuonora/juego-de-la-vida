8c2b4699fa88f35cb845467474459bbdd0206814
rafa <rlabuonora@yahoo.com>
2020-02-04 17:40:06 -0300

    Termina vecinos vivos

--- src/Grid.js
+++ src/Grid.js
@@ -3,33 +3,47 @@ class Grid {
         this.data = data;
         this.height = data.length;
         this.width = data[0].length; // falla si pasa un ragged array
-        // set reference to this
-        this.data.map( row => row.map(cell => cell.grid = this));
     }
-
-    cells(i, j) {
+    estado(i, j) {
         return this.data[i][j];
     }
-    next() {
-        // TODO implement
-        return new Grid(this.data);
-    }
-}
+    vecinos_vivos(i, j) { // devuelve la cantidad de vecinas vivos  de i, j
 
-class Cell {
-    constructor(on) {
-        this.state = on; // use setter & getter?
-    }
-    neighbors() {
-        var offsets = [-1, 0, 1]
+        // todos los posibles offsets de la celda i, j
+        return [-1, 0, 1]
             .map(row_offset => [-1, 0, 1]
                 .map(col_offset => [row_offset, col_offset]))
-                .reduce((acc, current) => acc.concat(current));
-        return offsets;
+            .reduce((acc, current) => acc.concat(current))
+            // sacar (0, 0)
+            .filter(([row_offset, col_offset]) => ! (row_offset === 0 &
+                                                     col_offset === 0))
+            // sacar los que estan afuera de la grid
+            .filter(([row_offset, col_offset]) => (i + row_offset >= 0) &
+                                                (i + row_offset < this.height))
+            .filter(([row_offset, col_offset]) => (j + col_offset >= 0) &
+                                                (j + col_offset < this.width))
+            // solo los vivos
+            .filter(([row_offset, col_offset]) => this.data[i+row_offset][j+col_offset])
+            .length;
+        
+        
+    }
+    siguiente() {
+        // TODO implement
+        // estaria bueno tener un metodo para iterar por las celdas
+        // nested for es la peor opcion
+        // para cada celda (acumulo en new_data)
+        // para esto no estaria mal que cada cell supiera su i, j
+        let new_data = [[]];
+        // contar cuantos vecinos vivos tiene
+        // si son tres sigue viva
+        // si son dos sigue como esta
+        // si son menos de dos se muere 
+        return new Grid(new_data);
     }
 }
 
-            export { Grid, Cell };
+export { Grid };
 
 
 
--- src/Grid.test.js
+++ src/Grid.test.js
@@ -1,13 +1,7 @@
-import { Grid, Cell } from './Grid';
+import { Grid } from './Grid';
 
-
-let cell1 = new Cell(false);
-let cell2 = new Cell(false);
-let cell3 = new Cell(false);
-let cell4 = new Cell(false);
-
-let data = [[cell1, cell2],
-            [cell3, cell4]];
+let data = [[false, false],
+            [false, false]];
 
 
 let grid = new Grid(data);
@@ -15,74 +9,65 @@ let grid = new Grid(data);
 
 describe('Grid', () => {
 
-    test('Grid constructor', () => {
-        expect(grid.cells(0, 0)).toBe(cell1);
-    });
-
     test('Grid height & width', () => {
         expect(grid.height).toBe(2);
         expect(grid.width).toBe(2);
     });
 
 
-    test('Set cell state', () => {
-        grid.cells(0, 0).on = true;
-        expect(grid.cells(0, 0).on).toBe(true);
-    });
-});
-
-
-describe('Cell', () => {
-    test('Cell references parent grid', () => {
-        expect(cell1.grid).toBe(grid);
-        expect(grid.cells(0, 0).grid).toBe(grid);
+    xtest('Set cell state', () => {
+        //grid.estado(0, 0) = false;
         
     });
+
     test('Cells en la (0, 0) de una matriz 3 x 3', () => {
 
         // a 3 x 3 grid
         let grid = new Grid([
-            [new Cell(false), new Cell(false), new Cell(false)],
-            [new Cell(false), new Cell(false), new Cell(false)],
-            [new Cell(false), new Cell(false), new Cell(false)]
+            [false, false, false],
+            [false, false, true],
+            [false, false, true]
         ]);
-        const actual = grid.cells(0, 0).neighbors()
-        expect(actual.length).toBe(3);
+        expect(grid.vecinos_vivos(0, 0)).toBe(0);
+        expect(grid.vecinos_vivos(1, 1)).toBe(2);
+        expect(grid.vecinos_vivos(2, 0)).toBe(0);
+        expect(grid.vecinos_vivos(0, 2)).toBe(1);
         
     });
 
-    test('Cells en la (1, 1) de una matriz 3 x 3', () => {
+    xtest('Cells en la (1, 1) de una matriz 3 x 3', () => {
 
         // a 3 x 3 grid
         let grid = new Grid([
-            [new Cell(false), new Cell(false), new Cell(false)],
-            [new Cell(false), new Cell(false), new Cell(false)],
-            [new Cell(false), new Cell(false), new Cell(false)]
+            [false, false, false],
+            [false, false, false],
+            [false, false, false]
         ]);
+
         const actual = grid.cells(1, 1).neighbors()
         expect(actual.length).toBe(8);
         
     });
 
-    test('Cells en la (2, 2) de una matriz 3 x 3', () => {
+    xtest('Cells en la (2, 2) de una matriz 3 x 3', () => {
 
         // a 3 x 3 grid
         let grid = new Grid([
-            [new Cell(false), new Cell(false), new Cell(false)],
-            [new Cell(false), new Cell(false), new Cell(false)],
-            [new Cell(false), new Cell(false), new Cell(false)]
+            [false, false, false],
+            [false, false, false],
+            [false, false, false]
         ]);
         const actual = grid.cells(2, 2).neighbors()
         expect(actual.length).toBe(3);
         
     });
 
-    test('Cells en la 0, 1 de una matriz 3 x 3', () => {
-        // a 3 x 3 grid
+    xtest('Cells en la 0, 1 de una matriz 3 x 3', () => {
+        
         let grid = new Grid([
-            [new Cell(false), new Cell(false), new Cell(false)],
-            [new Cell(false), new Cell(false), new Cell(false)],
-            [new Cell(false), new Cell(false), new Cell(false)]
+            [false, false, false],
+            [false, false, false],
+            [false, false, false]
         ]);
         const actual = grid.cells(1, 1).neighbors()
         console.log(actual);
