5589d1b46f902f6b7f555643efac642020a7d891
rafa <rlabuonora@yahoo.com>
2020-02-05 13:48:56 -0300

    Termina Grid.siguiente()

--- src/Grid.test.js
+++ src/Grid.test.js
@@ -3,9 +3,7 @@ import { Grid } from './Grid';
 let data = [[false, false],
             [false, false]];
 
-
 let grid = new Grid(data);
-
 
 describe('Grid', () => {
 
@@ -15,12 +13,76 @@ describe('Grid', () => {
     });
 
 
-    xtest('Set cell state', () => {
-        //grid.estado(0, 0) = false;
+    test('Grid.get_estado', () => {
+        
+        let data_1 = [[false, false],
+                      [false, true]];
+
+
+        let grid_1 = new Grid(data_1);
+                
+        expect(grid_1.get_estado(1, 1)).toBe(true);
+        
         
     });
 
-    test('Cells en la (0, 0) de una matriz 3 x 3', () => {
+    test('Grid.equal con arrays iguales', () => {
+        
+        let data_1 = [[false, false],
+                      [false, true]];
+
+        let data_2 = [[false, false],
+                      [false, true]];
+
+        let grid_1 = new Grid(data_1);
+        let grid_2 = new Grid(data_2);
+        
+        expect(grid_1.equals(grid_2)).toBe(true);
+        
+    });
+
+    test('Grid.equal con despues de cambiar', () => {
+        
+        let data_1 = [[false, true],
+                      [false, false]];
+
+        let data_2 = [[false, true],
+                      [false, false]];
+
+        let grid_1 = new Grid(data_1);
+        let grid_2 = new Grid(data_2);
+        
+        grid_1.set_estado(0, 0, true);
+
+        expect(grid_1.equals(grid_2)).toBe(false);
+        
+    });
+
+    test('Grid.set_estado', () => {
+        
+        let data_1 = [[false, false],
+                      [false, true]];
+
+        let grid_1 = new Grid(data_1);
+        // cambiar 1, 1 a false
+        grid_1.set_estado(1, 1, false)
+        expect(grid_1.get_estado(1, 1)).toBe(false);
+    });
+
+    test('Grid copia el array', () => {
+        
+        let data_1 = [[false, false],
+                      [false, false]];
+
+        let grid_1 = new Grid(data_1);
+        // antes de cambiar el array
+        expect(grid_1.get_estado(1, 1)).toBe(false);
+        // dps de cambiar el array
+        data_1[1][1] = true;
+        expect(grid_1.get_estado(1, 1)).toBe(false);
+    });
+
+    test('Grid.vecinos_vivos', () => {
 
         // a 3 x 3 grid
         let grid = new Grid([
@@ -32,10 +94,9 @@ describe('Grid', () => {
         expect(grid.vecinos_vivos(1, 1)).toBe(2);
         expect(grid.vecinos_vivos(2, 0)).toBe(0);
         expect(grid.vecinos_vivos(0, 2)).toBe(1);
-        
     });
 
-    xtest('Cells en la (1, 1) de una matriz 3 x 3', () => {
+    test('Grid.siguiente() vacia', () => {
 
         // a 3 x 3 grid
         let grid = new Grid([
@@ -44,9 +105,71 @@ describe('Grid', () => {
             [false, false, false]
         ]);
 
-        const actual = grid.cells(1, 1).neighbors()
-        expect(actual.length).toBe(8);
+        const actual = grid.siguiente();
+        expect(actual.equals(grid)).toBe(true);
+    });
+
+    test('Grid.siguiente() block', () => {
+
+        // a 4 x 4 grid
+        let grid = new Grid([
+            [false, false, false, false],
+            [false, true, true, false],
+            [false, true, true, false],
+            [false, false, false, false]
+        ]);
+
+        const actual = grid.siguiente();
+        expect(actual.equals(grid)).toBe(true);
+    });
+
+    test('Grid.siguiente() oscilator', () => {
+
+        // a 4 x 4 grid
+        let grid = new Grid([
+            [false, false, false, false, false],
+            [false, false, true,  false, false],
+            [false, false, true,  false, false],
+            [false, false, true,  false, false],
+            [false, false, false, false, false]
+        ]);
+
+        let new_grid = new Grid([
+            [false, false, false, false, false],
+            [false, false, false, false, false],
+            [false, true,  true,  true, false],
+            [false, false, false, false, false],
+            [false, false, false, false, false]
+        ]);
+
+        const actual = grid.siguiente();
+        expect(actual.equals(new_grid)).toBe(true);
+    });
+
+    test('Grid.siguiente() beacon', () => {
+
         
+        let grid = new Grid([
+            [false, false, false, false, false, false],
+            [false, true, true, false, false, false],
+            [false, true, true, false, false, false],
+            [false, false, false, true, true, false],
+            [false, false, false, true, true, false],
+            [false, false, false, false, false, false]
+        ]);
+
+        let new_grid = new Grid([
+            [false, false, false, false, false, false],
+            [false, true, true, false, false, false],
+            [false, true, false, false, false, false],
+            [false, false, false, false, true, false],
+            [false, false, false, true, true, false],
+            [false, false, false, false, false, false]
+        ]);
+        
+
+        const actual = grid.siguiente();
+        expect(actual.equals(new_grid)).toBe(true);
     });
 
     xtest('Cells en la (2, 2) de una matriz 3 x 3', () => {
