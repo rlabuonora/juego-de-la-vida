11c1cb403059bc4604a08792e9b6eda4f27a5169
Rafael La Buonora <info@rlabuonora.com>
2020-02-08 18:03:04 -0300

    Termina de enganchar los botones

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,16 @@ import Board from './Board';
 import Grid from './Grid';
 import './App.css';
 
+// Global consts
+const HEIGHT = 8;
+const WIDTH = 8;
+const SPEED = 500;
+
 class App extends React.Component {
     constructor(props) {
         super(props);
         // inicizar parametros
-        const HEIGHT = 8;
-        const WIDTH = 8;
+
         // inicializar grid
         const data = Array(HEIGHT).fill(Array(WIDTH).fill(false));
         const grid = new Grid(data);
@@ -17,6 +21,7 @@ class App extends React.Component {
         this.state = {
             grid: grid,
             generacion: 0,
+            intervalID: null,
             on: false
         };
     }
@@ -25,33 +30,74 @@ class App extends React.Component {
             grid: this.state.grid.toggle_cell(i, j)
         })
     }
+    handlePlay() {
+        if (this.state.on) {
+            this.pause();
+        } else {
+            this.start();
+        }
+    }
+    stop() {
+        // inicializar grid
+        const data = Array(HEIGHT).fill(Array(WIDTH).fill(false));
+        const grid = new Grid(data);
+        clearInterval(this.state.intervalId);
+        this.setState({
+            on: false,
+	    generacion: 0,
+	    grid: grid,
+        });
+    }
+    start() {
+        var intervalId = setInterval(() => this.tick(),
+				     SPEED);
+        this.setState({ on: true,
+	                intervalId: intervalId });
+    }
+
+    pause() {
+        clearInterval(this.state.intervalId);
+        this.setState({ on: false });
+    }
+
+    tick() {
+        console.log('tick');
+        var generacion = this.state.generacion + 1;
+        var grid = this.state.grid;
+
+        this.setState({
+	    generacion : generacion,
+            grid: grid.siguiente()
+        });
+    }
     render() {
         return (
                 <div className="App">
-        	        <div className="col-lg-6">
-	        <div className="row">
-	        <span className="generacion">
-                Generación: {this.state.t}
-                </span>
-                <span className="botones">
-	        <PlayButton onClick={this.handlePlay.bind(this)}
-            on={this.state.on}
-	    className="play" />
-                <StopButton className="stop" stop={this.stop.bind(this)} />
-	        </span>
-	        </div>
-
-                <Board
-                  grid={this.state.grid}
-            onClick={(i, j) =>  this.handleClick(i, j)} />
+        	    <div className="col-lg-6">
+	                <div className="row">
+	                    <span className="generacion">
+                                Generación: {this.state.generacion}
+                            </span>
+                            <span className="botones">
+	                        <PlayButton on={this.state.on}
+                                            onClick={()=> this.handlePlay()}
+                                            className="play" />
+                <StopButton onClick={() => this.stop()}
+                            className="stop" />
+	                    </span>
+	                </div>
+                    </div>
+                    <div class="board">
+                        <Board grid={this.state.grid}
+                            onClick={(i, j) =>  this.handleClick(i, j)} />
+                    </div>
+                </div>
         );
     }
 }
 
-// TODO: Mover a otro archivo
-
+// TODO: Mover a otro archivo y convertir en funciones
 class PlayButton extends Component {
-
     icon() {
         return this.props.on ?
 	    <i className="fa fa-pause" aria-hidden="true"></i> :
@@ -59,8 +105,7 @@ class PlayButton extends Component {
     }
     render() {
         return (
-                <button onClick={ /* TODO */ }
-	                className="btn btn-secondary"> {this.icon() }
+                <button onClick={this.props.onClick} className="btn btn-secondary"> {this.icon() }
 	        </button>
         );
     }
@@ -69,7 +114,7 @@ class PlayButton extends Component {
 class StopButton extends Component {
     render() {
         return (
-                <button onClick={this.props.stop}
+                <button onClick={this.props.onClick}
 	                className="btn btn-secondary">
 	            <i className="fa fa-stop" aria-hidden="true"></i>
 	        </button>
@@ -79,4 +124,3 @@ class StopButton extends Component {
 
 
 export default App;
-n
